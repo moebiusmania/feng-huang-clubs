a854e6546c03775c07353c9faa16bf816fa5db54
Salvatore Laisa <moebiusmania@gmail.com>
2017-08-29 23:22:04 +0200

    start Polymer v3 & NPM workflow refactor (#7)
    * start refactor for polymer v3 & NPM
    
    * add readme, updated license

---
+++ readme.md
@@ -0,0 +1,21 @@
+# &lt;fh-clubs&gt;
+
+Small application that acts as a gym locator for the Feng Huang school clubs, built on top of Polymer & Web Components.
+
+## --Work in progress!--
+
+## Install & develop locally
+Requires:
+* Node.js 8.x
+* Yarn
+
+clone the repo and then from the command line:
+
+* ```$ yarn``` to install dependencies
+* ```$ yarn dev``` to start development task
+* open a browser on ```localhost:8081```
+
+have fun!
+
+## License
+Released under the [MIT license](LICENSE).
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+const WebpackNotifierPlugin = require('webpack-notifier');
+// const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: 'app.js',
+  },
+  devtool: 'source-map',
+  plugins: [
+    // new UglifyJSPlugin({
+    //   sourceMap: true
+    // }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
+    }),
+    new WebpackNotifierPlugin({
+      title: 'Feng Huang Clubs',
+      alwaysNotify: true
+    })
+  ],
+  module: {
+    rules: [{
+      test: /\.html$/,
+      use: 'text-loader'
+    },{
+      test: /\.js?$/,
+      exclude: ['node_modules', './dist/app.js'],
+      loader: 'babel-loader',
+      query: {
+        presets: []
+      }
+    }]
+  }
+}
